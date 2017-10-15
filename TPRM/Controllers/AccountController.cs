20c169293532d8d20a11ed04c28a2e9aa8c6f58f
MarcMelo <marcilio_sergio@hotmail.com>
2017-10-15 18:32:25 -0300

    ajuste final.

--- TPRM/Controllers/AccountController.cs
+++ TPRM/Controllers/AccountController.cs
@@ -166,7 +166,7 @@ namespace TPRM.Controllers
                     { ApplicationUserID = user.Id, EmpresaID = model.EmpresaID };
                     UsuarioEmpresasController.Create(usuarioEmpresa, db);
 
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    //await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
